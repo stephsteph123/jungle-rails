00b3f52781a9eb966d2942343716021cba13a62f
stephsteph123 <stephaniepotterrowe@gmail.com>
2023-01-31 16:59:20 -0500

    update to user auth process

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with :name => ENV['secret_user'], :password => ENV['secret_password']
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+
 
   def index
     @products = Product.order(id: :desc).all
